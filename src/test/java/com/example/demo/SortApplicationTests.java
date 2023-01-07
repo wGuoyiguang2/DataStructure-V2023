4f3a7f2c031561c4b3df3b2144b41201a49015f0
guoyg <@>
2023-01-07 19:23:19 +0800

    二分查找(递归实现)

--- src/test/java/com/example/demo/SortApplicationTests.java
+++ src/test/java/com/example/demo/SortApplicationTests.java
@@ -55,10 +55,17 @@ public class SortApplicationTests {
        // mergeSort();
 
         // 快速排序(用到了双指针算法，不需要开辟新的内存空间)
-        int[] array = {11,4,8,2,10 };
-        int[] ints = quickSort(array, 0, 4);
-        System.out.println(Arrays.toString(ints));
-        System.out.println(Arrays.toString(ints));
+//        int[] array = {11,4,8,2,10 };
+//        int[] ints = quickSort(array, 0, 4);
+//        System.out.println(Arrays.toString(ints));
+//        System.out.println(Arrays.toString(ints));
+
+
+        // 折半查找(二分查找) ：前提：数据是已经提前排好顺序的
+
+        int[] arraybinarySearch = {1,3,5,7,9,10 };
+        int binarySearchIndex = binarySearch(arraybinarySearch, 0, 6, 10);
+        System.out.println(binarySearchIndex);
 
 
     }
@@ -522,6 +529,87 @@ public class SortApplicationTests {
 
     }
 
+    /**
+     * 功能描述 : 二分查找(折半查找)
+     *
+     *   应用：查找某个数据是否存在于数组，但是如果 直接for 循环遍历，时间复杂度  是 O(n)
+     *        但是如果是 二分查找的话，时间复杂度是  O(logn)
+     * @author guoyiguang
+     * @date 2023/1/7
+     * @param
+     * @return
+     */
 
+
+    /**
+     * 功能描述 : 递归实现 二分查找法
+     *                 重复操作：每次将子区间折半
+     *                 终止条件：待折半区间 长度为1（或者只有一个元素终止递归）
+     * @author guoyiguang
+     * @date 2023/1/7
+     * @param
+     * @return
+     */
+    public int  binarySearch(int[] array ,int  start ,int end,int target){
+
+        // target = 3
+        //  1,3,5,7,9
+
+
+
+
+        // 递归终止条件  ①
+        if(start == end ){
+            // 和目标值 target 比较，一样，则返回下标，否则返回 -1
+            if(array[start] == target){
+                return start;
+            }
+            return -1;
+        }
+
+        //  递归终止条件  ②
+        int middle = (start+end)/2;
+        // 刚好等于目标值
+        if(array[middle] == target){
+            return middle;
+            // 中间值比目标值大，取数组 左边重新折半
+        }else if(array[middle] > target){
+            // target = 3 ,start = 0;middle-1 = end = 1
+            // 错误写法 ①①：
+            // binarySearch( array ,  start ,middle-1, target);
+            // 正确写法 是要加 return 的
+          return   binarySearch( array ,  start ,middle-1, target);
+            // 中间值比目标值小，取数组 右边重新折半
+        }else{
+            // target = 3 ,start = middle+1 = 1; end = 1
+            // 错误写法 ①②：
+            // binarySearch( array ,  middle+1 ,end, target);
+            // 正确写法： 需要加个 return
+            return   binarySearch( array ,  middle+1 ,end, target);
+        }
+
+        // 错误写法：
+        // 错误原因：  上面①① 和 ①② 是没有 return 关键字的，假如 程序找到了 target 然后  return start，这个
+        //          binarySearch( array ,  start ,middle-1, target)
+        //          or binarySearch( array ,  middle+1 ,end, target)
+        //          只是代表 这两个语句是执行完了，但是 走到最后  还有 下面的语句 return -1 没有走，于是程序就走了
+        //          return -1
+        // return  -1;
+
+
+    }
+
+
+
+
+        /**
+         * 功能描述 : 线性排序 ：桶排序，计数排序和基数排序（时间复杂度 都是 O(n)）
+         *
+         * 之所以能做到线性的时间复杂度，主要是因为它们都不是基于比较的排序算法，排序的过程不涉及元素之间的比较操作。
+         * @author guoyiguang
+         * @date 2023/1/7
+         * @param
+         * @return
+         */
 
 }
