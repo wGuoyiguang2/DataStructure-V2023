5b74e8e38c9bea73ddb8c6e2ce001ef09c204e68
guoyg <@>
2023-01-09 13:40:26 +0800

    digui

--- src/test/java/com/example/demo/RecursionAppicationTest.java
+++ src/test/java/com/example/demo/RecursionAppicationTest.java
@@ -101,6 +101,65 @@ public class RecursionAppicationTest {
 
 
 
+    /**
+     * 功能描述 : 递归实现 二分查找法
+     *                 重复操作：每次将子区间折半
+     *                 终止条件：待折半区间 长度为1（或者只有一个元素终止递归）
+     * @author guoyiguang
+     * @date 2023/1/7
+     * @param
+     * @return
+     */
+    public int  binarySearch(int[] array ,int  start ,int end,int target){
+
+        // target = 3
+        //  1,3,5,7,9
+
+
+        // 递归终止条件  ①
+        if(start == end ){
+            // 和目标值 target 比较，一样，则返回下标，否则返回 -1
+            if(array[start] == target){
+                return start;
+            }
+            return -1;
+        }
+
+        //  递归终止条件  ②
+        int middle = (start+end)/2;
+        // 刚好等于目标值
+        if(array[middle] == target){
+            return middle;
+            // 中间值比目标值大，取数组 左边重新折半
+        }else if(array[middle] > target){
+            // target = 3 ,start = 0;middle-1 = end = 1
+            // 错误写法 ①①：
+            // binarySearch( array ,  start ,middle-1, target);
+            // 正确写法 是要加 return 的
+            return   binarySearch( array ,  start ,middle-1, target);
+            // 中间值比目标值小，取数组 右边重新折半
+        }else{
+            // target = 3 ,start = middle+1 = 1; end = 1
+            // 错误写法 ①②：
+            // binarySearch( array ,  middle+1 ,end, target);
+            // 正确写法： 需要加个 return
+            return   binarySearch( array ,  middle+1 ,end, target);
+        }
+
+        // 错误写法：
+        // 错误原因：  上面①① 和 ①② 是没有 return 关键字的，假如 程序找到了 target 然后  return start，这个
+        //          binarySearch( array ,  start ,middle-1, target)
+        //          or binarySearch( array ,  middle+1 ,end, target)
+        //          只是代表 这两个语句是执行完了，但是 走到最后  还有 下面的语句 return -1 没有走，于是程序就走了
+        //          return -1
+        // return  -1;
+
+
+    }
+
+
+
+
 
 
 
