6ab7b5251acdd334a12782b4fd139dff741f8a51
guoyg <@>
2023-01-12 19:41:27 +0800

    树遍历

--- src/test/java/com/example/demo/LinkedListApplicationTests.java
+++ src/test/java/com/example/demo/LinkedListApplicationTests.java
@@ -24,12 +24,45 @@ public class LinkedListApplicationTests {
 
 
 
+    /**
+     * 功能描述：  链表 删除 一个元素
+     * @author guoyiguang
+     * @date 2023/1/12
+     * @param
+     * @return
+     */
+    @Test
+    public void  linkTest() {
+
+        MyNode myNode1 = new MyNode();
+        myNode1.setValue("myNode1");
+        MyNode myNode2 = new MyNode();
+        myNode2.setValue("myNode2");
+        MyNode myNode3 = new MyNode();
+        myNode3.setValue("myNode3");
+
+        MyNode myNode4 = new MyNode();
+        myNode4.setValue("myNode4");
+
+        //  形成链
+        myNode1.setNext(myNode2);
+        myNode2.setNext(myNode3);
+        myNode3.setNext(myNode4);
+
+        deleteElementToLink(myNode1,myNode1);
+
+
+
+
+    }
+
+
 
 
 
     /**
      *  遍历一个链表
-     *  插入，删除一个元素
+     *
      */
     @Test
     public void ergodic() {
@@ -76,13 +109,127 @@ public class LinkedListApplicationTests {
                 System.out.println(temFor);
                 temFor  = temFor.getNext();
             }else{
-                break;
+                //break;
+                // 也可以替换 return
+                return ;
             }
 
         }
 
 
     }
+
+    /**
+     * 功能描述：  链表插入一个元素
+     * @author guoyiguang
+     * @date 2023/1/12
+     * @param
+     * @return
+     */
+    @Test
+    public void insertElementToLink() {
+
+        MyNode myNode1 = new MyNode();
+        myNode1.setValue("myNode1");
+        MyNode myNode2 = new MyNode();
+        myNode2.setValue("myNode2");
+        MyNode myNode3 = new MyNode();
+        myNode3.setValue("myNode3");
+
+        //  形成链
+        myNode1.setNext(myNode2);
+        myNode2.setNext(myNode3);
+        // 这一步可以省去不写
+        myNode3.setNext(null);
+
+        // 在 myNode2 和  myNode3 直接插入 myNode4
+        MyNode myNode4 = new MyNode();
+        myNode4.setValue("myNode4");
+
+        myNode2.setNext(myNode4);
+
+        myNode4.setNext(myNode3);
+
+
+
+
+    }
+
+
+    /**
+     *   TODO :  第一个和最后一个需要处理
+     * 功能描述：  链表 删除 一个元素
+     *
+     *
+     *
+     * 思路：假如链表 A---> B ---> C , 要删除 B 元素
+     *     先在链表找到B元素的前置元素A和后置元素c
+     *     然后 A.next(C) ,B.next(null)
+     *     最后
+     *       A ---> C
+     *          B
+     *
+     * @author guoyiguang
+     * @date 2023/1/12
+     * @param
+     * @return
+     */
+    public void deleteElementToLink(MyNode firstNode,MyNode  deleteNode) {
+
+        // 设置临时变量存储 前置节点
+        MyNode  preposition = firstNode ;
+        // 设置临时变量存储 后置节点
+        MyNode  postposition =  firstNode ;
+
+
+
+        // A---> B ---> C
+        // 遍历链表 查找 前置节点，目标节点和后置节点
+        MyNode  tem =  firstNode;
+        while(null != tem){
+            if(tem == deleteNode){
+                postposition = tem.getNext();
+                // 赋值成功，结束循环
+                break ;
+                // return 不但结束循环，而且也跳出方法体 （这儿不可以用return ）
+            }else{
+                //  preposition 指向 tem 指向的对象（在堆里）
+                preposition = tem;
+            }
+            //   tem  指向  tem 的 next 对象
+            tem = tem.getNext();
+
+        }
+        System.out.println(preposition);
+        System.out.println(postposition);
+        System.out.println("--");
+
+        // 改变 引用
+        preposition.setNext(postposition);
+        deleteNode.setNext(null);
+
+        // 遍历链表
+        MyNode  tem2 =  firstNode;
+        System.out.println("遍历删除元素后的链表");
+        while(null != tem2){
+            System.out.println(tem2);
+            // A--->  B --->  C --->   D
+            //
+            //   tem  指向  tem 的 next 对象
+            tem2 = tem2.getNext();//  tem.getNext() 这个引用 指向 堆的一个对象Object1 ，此条语句是将 tem 这个引用 指向 Object1
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
 
 
     @Test
